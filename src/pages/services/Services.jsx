6c391a5e5b5ccb1204cfeece8d87dc76dd43adf8
Gaimar23 <highdevelopment01@gmail.com>
2024-09-04 03:28:27 +0200

    seventh Commit

--- src/pages/services/Services.jsx
+++ src/pages/services/Services.jsx
@@ -1,12 +1,89 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Services.scss";
 import Navbar from "../../components/navbar/Navbar";
+import Welcome from "../../components/welcome/Welcome";
+import service01 from "../../assets/images/project1.png";
+import service02 from "../../assets/images/project2.png";
+import service03 from "../../assets/images/project3.png";
+import { MdOutlineSecurity } from "react-icons/md";
+import { CgPerformance } from "react-icons/cg";
+import { FaPersonRunning } from "react-icons/fa6";
+import { TbStackForward } from "react-icons/tb";
 
 const Services = () => {
+  const [aWebsite, setAWebsite] = useState("");
+
+  useEffect(() => {
+    setAWebsite(document.getElementById("a-website"));
+  }, []);
+
+  const handleWebsite = () => {
+    aWebsite.parentElement.classList.contains("open")
+      ? aWebsite.parentElement.classList.remove("open")
+      : aWebsite.parentElement.classList.add("open");
+  };
+
   return (
     <div className="services">
       <Navbar />
-      <h1>Services</h1>
+      <Welcome text="Des services taillées pour vos besoins" />
+      <div className="our-services">
+        <div className="services-container">
+          <div className="item">
+            <div className="top" id="a-website" onClick={handleWebsite}>
+              <div className="image-container">
+                <img src={service01} alt="" className="service-image" />
+              </div>
+              <h5 className="title">Conception d'un Site Web</h5>
+              <button>Commander</button>
+            </div>
+            <div className="bottom">
+              <h5>Conception d'un Site Web</h5>
+              <div className="details">
+                <div className="left">
+                  <p>
+                    Vous désirez un site web qui combine la performance, la
+                    rapidité, la sécurité...etc Nous récourrons aux technologies
+                    les plus récentes pour donner corps à votre vision ou
+                    projet, vous délivrer des solutions qui réflectent votre
+                    besoin. Qu'il s'agisse d'un site statique ou dynamique,
+                    notre équipe d'expert doté de plusieurs années d'expérience
+                    s'atèle à la tâche pour vous délivrer une solution Web dans
+                    les temps impartis. Nous vous rémercions par avance de la
+                    confiance que vous nous témoignez en nous confiant votre
+                    projet.
+                  </p>
+                </div>
+                <div className="right">
+                  <p>Un site Web </p>
+                  <div className="options">
+                    <div className="option">
+                      <span>Performant</span>{" "}
+                      <CgPerformance className="option-icon" />
+                    </div>
+                    <div className="option">
+                      <span>Rapide</span>
+                      <FaPersonRunning className="option-icon" />
+                    </div>
+                    <div className="option">
+                      <span>Sécurisé</span>
+                      <MdOutlineSecurity className="option-icon" />
+                    </div>
+                    <div className="option">
+                      <span>Evolutif</span>
+                      <TbStackForward className="option-icon" />
+                    </div>
+                    <div className="option">
+                      <span>Performant</span>{" "}
+                      <CgPerformance className="option-icon" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
