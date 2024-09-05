41002a1988ea6658a73d49df81f6e5a29c35608a
Gaimar23 <highdevelopment01@gmail.com>
2024-09-05 16:13:52 +0200

    eighth Commit

--- src/pages/services/Services.jsx
+++ src/pages/services/Services.jsx
@@ -9,18 +9,26 @@ import { MdOutlineSecurity } from "react-icons/md";
 import { CgPerformance } from "react-icons/cg";
 import { FaPersonRunning } from "react-icons/fa6";
 import { TbStackForward } from "react-icons/tb";
+import Footer02 from "../../components/footer02/Footer02";
 
 const Services = () => {
   const [aWebsite, setAWebsite] = useState("");
+  const [mobileApp, setMobileApp] = useState("");
 
   useEffect(() => {
     setAWebsite(document.getElementById("a-website"));
+    setMobileApp(document.getElementById("mobile-app"));
   }, []);
 
   const handleWebsite = () => {
     aWebsite.parentElement.classList.contains("open")
       ? aWebsite.parentElement.classList.remove("open")
       : aWebsite.parentElement.classList.add("open");
+  };
+  const handleMobile = () => {
+    mobileApp.parentElement.classList.contains("open")
+      ? mobileApp.parentElement.classList.remove("open")
+      : mobileApp.parentElement.classList.add("open");
   };
 
   return (
@@ -39,6 +47,128 @@ const Services = () => {
             </div>
             <div className="bottom">
               <h5>Conception d'un Site Web</h5>
+              <div className="details">
+                <div className="left">
+                  <p className="one">
+                    Pour vous délivrer des solutions qui réflectent votre besoin
+                    et s'intégrent à votre stratégie, Nous récourrons aux
+                    technologies les plus récentes.
+                  </p>
+                  <p className="two">
+                    Qu'il s'agisse d'un site statique ou dynamique, notre équipe
+                    d'expert doté de plusieurs années d'expérience s'atèle à la
+                    tâche pour vous offrir la meilleure des solution Web dans
+                    les temps impartis
+                  </p>
+                </div>
+                <div className="right">
+                  <div className="options-container">
+                    <p>Un site Web </p>
+                    <div className="options">
+                      <div className="option">
+                        <span>Performant</span>{" "}
+                        <CgPerformance className="option-icon" />
+                      </div>
+                      <div className="option">
+                        <span>Rapide</span>
+                        <FaPersonRunning className="option-icon" />
+                      </div>
+                      <div className="option">
+                        <span>Sécurisé</span>
+                        <MdOutlineSecurity className="option-icon" />
+                      </div>
+                      <div className="option">
+                        <span>Evolutif</span>
+                        <TbStackForward className="option-icon" />
+                      </div>
+                      <div className="option">
+                        <span>Performant</span>{" "}
+                        <CgPerformance className="option-icon" />
+                      </div>
+                    </div>
+                  </div>
+                  {/* <p>
+                    Nous vous rémercions par avance de la confiance que vous
+                    nous témoignez en nous confiant votre projet.
+                  </p> */}
+                  <div className="btn-container">
+                    <button className="order-btn">Commander</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="top" id="mobile-app" onClick={handleMobile}>
+              <div className="image-container">
+                <img src={service01} alt="" className="service-image" />
+              </div>
+              <h5 className="title">Conception d'Appli Mobile</h5>
+              <button>Commander</button>
+            </div>
+            <div className="bottom">
+              <h5>Conception d'Appli Mobile</h5>
+              <div className="details">
+                <div className="left">
+                  <p className="one">
+                    Pour vous délivrer des solutions qui réflectent votre besoin
+                    et s'intégrent à votre stratégie, Nous récourrons aux
+                    technologies les plus récentes.
+                  </p>
+                  <p className="two">
+                    Qu'il s'agisse d'un site statique ou dynamique, notre équipe
+                    d'expert doté de plusieurs années d'expérience s'atèle à la
+                    tâche pour vous offrir la meilleure des solution Web dans
+                    les temps impartis
+                  </p>
+                </div>
+                <div className="right">
+                  <div className="options-container">
+                    <p>Une Appli Mobile</p>
+                    <div className="options">
+                      <div className="option">
+                        <span>Performant</span>{" "}
+                        <CgPerformance className="option-icon" />
+                      </div>
+                      <div className="option">
+                        <span>Rapide</span>
+                        <FaPersonRunning className="option-icon" />
+                      </div>
+                      <div className="option">
+                        <span>Sécurisé</span>
+                        <MdOutlineSecurity className="option-icon" />
+                      </div>
+                      <div className="option">
+                        <span>Evolutif</span>
+                        <TbStackForward className="option-icon" />
+                      </div>
+                      <div className="option">
+                        <span>Performant</span>{" "}
+                        <CgPerformance className="option-icon" />
+                      </div>
+                    </div>
+                  </div>
+                  {/* <p>
+                    Nous vous rémercions par avance de la confiance que vous
+                    nous témoignez en nous confiant votre projet.
+                  </p> */}
+                  <div className="btn-container">
+                    <button className="order-btn">Commander</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="top" id="a-website" onClick={handleWebsite}>
+              <div className="image-container">
+                <img src={service01} alt="" className="service-image" />
+              </div>
+              <h5 className="title">Conception d'Appli Excel VBA</h5>
+              <button>Commander</button>
+            </div>
+            <div className="bottom">
+              <h5>Conception d'Appli Excel VBA</h5>
               <div className="details">
                 <div className="left">
                   <p>
@@ -84,6 +214,7 @@ const Services = () => {
           </div>
         </div>
       </div>
+      <Footer02 />
     </div>
   );
 };
