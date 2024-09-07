767378b463141c544a3cd40c0017d3871a4aeb22
Gaimar23 <highdevelopment01@gmail.com>
2024-09-07 12:59:33 +0200

    Seventeenth commit

--- src/autres/context/DataContext.jsx
+++ src/autres/context/DataContext.jsx
@@ -1,8 +1,8 @@
 import { createContext, useState } from "react";
 import Training1 from "../../assets/images/logo.png";
-import cours3 from "../../assets/images/cours/3.png";
-import cours4 from "../../assets/images/cours/4.png";
-import cours5 from "../../assets/images/cours/5.png";
+import cours3 from "../../assets/images/cours/1.png";
+import cours4 from "../../assets/images/cours/2.png";
+import cours5 from "../../assets/images/cours/3.png";
 import cours6 from "../../assets/images/cours/6.png";
 import cours7 from "../../assets/images/cours/7.png";
 
@@ -15,7 +15,7 @@ const DataContextProvider = (props) => {
     {
       image: cours3,
       title: "Excel",
-      desc: "Les Bases",
+      desc: "Tout savoir sur Excel",
     },
     {
       image: cours4,
@@ -25,17 +25,17 @@ const DataContextProvider = (props) => {
     {
       image: cours5,
       title: "Excel",
-      desc: "Développement VBA",
+      desc: "Macros & VBA",
     },
     {
       image: cours6,
       title: "Développement Web",
-      desc: "React, MongoDb, Node",
+      desc: "Full Stack",
     },
     {
       image: cours7,
-      title: "Design app Mobile",
-      desc: "React native, node",
+      title: "Développement",
+      desc: "Mobile",
     },
   ];
 
